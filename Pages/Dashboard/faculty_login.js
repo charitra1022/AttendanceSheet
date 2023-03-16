2ec37993ecdfd9ed374ed7a5c2bc01d1d4b769c0
Charitra Agarwal <charitraagarwal1022@gmail.com>
2023-03-16 16:05:46 +0530

    moved dashboard folder to Pages folder

---
+++ Pages/Dashboard/faculty_login.js
@@ -0,0 +1,31 @@
+// const teacher_id = 4;
+
+// function updateFacultyInfo() {
+//   const facultyNameEl = document.getElementById("faculty-name");
+//   const url = "http://localhost:5000/teacher?teacher_id=4";
+//   // const url = `http://localhost:5000/teacher?teacher_id=${teacher_id}`;
+//   const getParams = { teacher_id: 4 };
+//   // const options = {
+//   //   method: "GET",
+//   //   body: JSON.stringify(getParams),
+//   // };
+
+//   fetch("https://jsonplaceholder.typicode.com/posts/1")
+//     .then((res) => res.json())
+//     .then((data) => {
+//       console.log(data);
+//     });
+// }
+
+// window.onload = () => {
+//   updateFacultyInfo();
+// };
+
+
+
+
+// $(document).ready(function(){
+//   $("button").click(function(){
+//     $("#div1").load("demo_test.txt");
+//   });
+// });
