420e1f1cfea2f2a75bc165eaa4d41ece3f9a2f17
Charitra Agarwal <charitraagarwal1022@gmail.com>
2023-03-19 07:46:21 +0530

    refined fetch to asynchronous type and added fetch for each attendance date

--- Pages/js/attendance-list.js
+++ Pages/js/attendance-list.js
@@ -1,3 +1,9 @@
+// returns fetched response from API
+async function fetchData(url, options = {}) {
+  let response = await fetch(url, options);
+  return response.json();
+}
+
 const tableHead = `
 <div class="container">
   <div class="row">
@@ -13,14 +19,14 @@ const getAttendanceAdapter = (params) => {
   /*
     params = {
       date: string of date in format YYYY-MM-DD
-      totalStudents: integer denoting total student count in a course semester,
-      presentStudents: integer denoting total present students in a class
+      total: integer denoting total student count in a course semester,
+      present: integer denoting total present students in a class
     }
   */
 
   let date = params.date;
-  let totalStudents = params.totalStudents;
-  let presentStudents = params.presentStudents;
+  let totalStudents = params.total;
+  let presentStudents = params.present;
 
   const html = `
   <div class="card py-3 my-3">
@@ -37,13 +43,14 @@ const getAttendanceAdapter = (params) => {
 };
 
 // update the DOM with the fetched data of attendance
-function updatePageData(params) {
+async function updatePageData(params) {
+  // retrieve subject code and teacher id from URLParams
   const sub_code = params["subject_code"];
   const teacher_id = params["teacher_id"];
 
   document.getElementById("subject-code-heading").innerText = sub_code;
 
-  // post request options
+  // post request options for fetching date list for attendances
   const options = {
     method: "POST",
     headers: {
@@ -55,42 +62,49 @@ function updatePageData(params) {
     }),
   };
 
-  let isEmpty = false;
-
-  // fetch teacher data
+  // fetch attendance data for a teacher and a subject
   const url = `http://localhost:5000/attendence-date`;
-  fetch(url, options)
-    .then((res) => res.json())
-    .then((data) => {
-      // get list of attendance dates
-      const dates = data["dates"];
-      console.log(dates);
+  const data = await fetchData(url, options);
+  const att_dates = data["dates"];
 
-      // add all date cards to a single string
-      let attendanceCards = "";
-      attendanceCards += tableHead;
+  // if no attendance exists, return
+  if (att_dates.length === 0) return;
 
-      if(dates.length === 0) isEmpty = true;
-      
-      // run loop for each date and add the html card together
-      dates.forEach((date) => {
-        // data to send to the card function for displaying information
-        const att_params = {
-          date: date,
-          totalStudents: 113,
-          presentStudents: 10,
-        };
+  // store list of objects of the attendance details
+  let att_details = [];
 
-        // add the html card to the string
-        attendanceCards += getAttendanceAdapter(att_params);
-      });
+  // run loop for each date and fetch data
+  for (const att_date of att_dates) {
+    let attData; // object to store date, total, present data
+    // post request options for each date request
+    const dateReqOpts = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        subject_code: sub_code,
+        teacher_id: teacher_id,
+        date: att_date,
+      }),
+    };
+    // url to request attendance details
+    const attDetailsUrl = `http://localhost:5000/attendence-details`;
+    const data = await fetchData(attDetailsUrl, dateReqOpts);
+    // add the attendance data to the list
+    attData = { ...data["resp"], date: att_date };
+    att_details.push(attData);
+  }
+  // add all date cards to a single string after data are fetched
+  let att_cards = "";
+  att_cards += tableHead; // add the heading for the table
 
-      // if there were some attendances, then only update the container in DOM
-      if(!isEmpty){
-        const attndnceContr = document.getElementById("attendance-list-container");
-        attndnceContr.innerHTML = attendanceCards;
-      }
-    });
+  att_details.forEach(
+    (att_detail) => (att_cards += getAttendanceAdapter(att_detail))
+  );
+
+  const attndnceContr = document.getElementById("attendance-list-container");
+  attndnceContr.innerHTML = att_cards;
 }
 
 window.onload = () => {
