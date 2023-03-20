48efc8f8ffa03861d96d57b7343d75dca97fe325
Charitra Agarwal <charitraagarwal1022@gmail.com>
2023-03-20 15:25:24 +0530

    added authtoken in login page

--- Pages/js/login.js
+++ Pages/js/login.js
@@ -3,19 +3,8 @@ const dashboardURL = "./facultyDashboard.html";
 
 const dummyId = {
   email: "mmgore@mnnit.ac.in",
-  password: "12345"
-}
-
-
-
-/*
- * Request reference: https://javascript.plainenglish.io/lets-build-a-website-login-page-with-html-css-javascript-and-an-external-api-a083942f797d
- */
-
-// var jwt = localStorage.getItem("jwt");
-// if (jwt != null) {
-//   window.location.href = dashboardURL;
-// }
+  password: "12345",
+};
 
 
 function login() {
@@ -27,34 +16,45 @@ function login() {
   const xhttp = new XMLHttpRequest();
   xhttp.open("POST", "http://localhost:5000/login-teacher");
   xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-  xhttp.send(JSON.stringify({
-    email: username,
-    password: password,
-  }));
-  
+  xhttp.send(
+    JSON.stringify({
+      email: username,
+      password: password,
+    })
+  );
+
   // after the request is done
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4) {
       // get the response
       const objects = JSON.parse(this.responseText);
 
-      console.log(objects);   // for debugging
+      console.log(objects); // for debugging
 
       // if the data of teacher is found
-      if (objects['success'] === true) {
-        // localStorage.setItem("jwt", objects['accessToken']);
-        
+      if (objects["success"] === true) {
+        window.localStorage.setItem("authToken", objects["authToken"]);
+
         // save teacher personal details to localStorage
-        const teacher_details = objects['result'][0]
-        window.localStorage.setItem("teacher_details", JSON.stringify(teacher_details));
+        const teacher_details = objects["result"][0];
+        window.localStorage.setItem(
+          "teacher_details",
+          JSON.stringify(teacher_details)
+        );
 
         window.location.href = dashboardURL;
-      } 
+      }
       // if the data of teacher is not found
       else {
-        alert(objects['error']);
+        alert(objects["error"]);
       }
     }
   };
   return false;
+}
+
+window.onload = () => {
+  // check if the auth token is present or not
+  if (window.localStorage.getItem("authToken") != null) 
+    window.location.href = dashboardURL;
 }
